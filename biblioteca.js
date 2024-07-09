b8e81396328da9a9d49f188475922b24ee897d28
Leachenski <jcleachenski@gmail.com>
2024-07-08 21:31:12 -0300

    feat: adiciona leitura e validação

--- biblioteca.js
+++ biblioteca.js
@@ -1,27 +1,53 @@
 const biblioteca = [];
+const generos = ["ação", "aventura", "ficção", "romance", "terror"];
+
+const validacao = (livro) => {
+  if (
+    livro.paginas > 0 &&
+    !isNaN(+livro.paginas) &&
+    generos.includes(livro.genero)
+  )
+    return true;
+  return false;
+};
 
 function create(livro) {
-    biblioteca.push(livro)
+    if(validacao(livro)) {
+        biblioteca.push(livro);
+        console.log("livro adicionado");
+    } else {
+        console.log("dados inválidos");
+    }
 }
 
 const read = () => {
-    biblioteca.forEach((livro, indice) => {
-        console.log(`${indice + 1} - ${livro}`)
-    })
-}
+  if (biblioteca.length === 0) console.log("Nenhum livro encontrado");
+  biblioteca.forEach((livro, indice) => {
+    console.log(indice + 1, livro);
+  });
+};
 
-const update = function(livro, indice) {
-    biblioteca[indice] = livro
-}
+const update = function (livro, indice) {
+  if (biblioteca[indice] && validacao(livro)) {
+    biblioteca[indice] = livro;
+    console.log("livro atualizado");
+  } else {
+    console.log("livro inexistente");
+  }
+};
 
-const destroy = indice => {
-    biblioteca.splice(indice, 1)
-}
+const destroy = (indice) => {
+  if (biblioteca[indice]) {
+    biblioteca.splice(indice, 1);
+    console.log("livro removido");
+  } else {
+    console.log("Livro inexistente");
+  }
+};
 
 module.exports = {
-    create,
-    read,
-    update,
-    destroy
-}
-
+  create,
+  read,
+  update,
+  destroy,
+};
